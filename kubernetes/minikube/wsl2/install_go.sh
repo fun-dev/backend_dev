35ca5f62d9407e66dd86878afbbff6ff8693c61c
materialofmouse <materialofmouse@gmail.com>
2020-10-26 18:13:17 +0900

    add: GOPATH

--- kubernetes/minikube/wsl2/install_go.sh
+++ kubernetes/minikube/wsl2/install_go.sh
@@ -1,6 +1,7 @@
 GO_CURRENT_VERSION=go1.15.3.linux-amd64.tar.gz
-
-
 curl -LO https://golang.org/dl/$GO_CURRENT_VERSION
-sudo tar -C /usr/local -xzf $GO_CURRENT_VERSION
+sudo tar -C /usr/local -xzvf $GO_CURRENT_VERSION
 rm $GO_CURRENT_VERSION
+GOPATH=/usr/local/go
+echo 'export PATH=$PATH:$GOPATH' > ~/.bashrc
+
